7d9d722573ae5a1e16e4c1d2def55245b321df62
mistertandon <enggparveshtandon@gmail.com>
2022-02-09 16:05:43 +0530

    feat: Extend WavePortal contract to include Wave struct data type.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,43 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal{
+
+    uint256 totalWaves;
+
+    event NewWave(address indexed from, uint256 timestamp , string message);
+
     constructor(){
         console.log('Hi from WavePortal contract');
+    }
+
+    struct Wave{
+        address waver;
+        string message;
+        uint256 timestamp;        
+    }
+
+    Wave[] waves;
+
+    function wave(string memory _message) public {
+        totalWaves +=1;
+        console.log("%s waved w/ message %s: ", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        console.log("New wave has been pushed from sender %s", msg.sender);
+        
+        emit NewWave(msg.sender, block.timestamp,_message);
+        console.log("Event NewWave has been emitted");
+    }
+
+    function getAllWaves() public view returns (Wave[] memory){
+
+        console.log("RETURN: getAllWaves: waves");
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256){
+
+        console.log("Retrieved total waves %d", totalWaves);
+        return totalWaves;   
     }
 }
